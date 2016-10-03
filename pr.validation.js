61d600170233221f81f8225fc81f3c913665b0b0
asp <dr.profee@yandex.ru>
2016-10-03 18:02:31 +0300

    added predefined

--- pr.validation.js
+++ pr.validation.js
@@ -92,6 +92,24 @@
                 } else {
                     return true;
                 }
+            },
+
+            email: function (element) {
+                var val = element.val();
+                if (val.match(/^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(\.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@([a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?\.)*(aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|[a-z][a-z])$/)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+
+            digits: function (element) {
+                var val = element.val();
+                if (val.match(/^[0-9]*$/)) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
 
         };
